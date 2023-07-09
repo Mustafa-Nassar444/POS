2609c4c40d691b10fb3363b9ad549c40be0824c5
Mustafa-Nassar444 <72405013+Mustafa-Nassar444@users.noreply.github.com>
2023-07-09 19:24:59 +0300

    first commit

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $user=User::create([
+           'first_name'=>'super',
+           'last_name'=>'admin',
+           'email'=>'nassar@app.superadmin',
+            'password'=>Hash::make('123321153')
+        ]);
+        $user->attachRole('super_admin');
+    }
+}
---
+++ routes/Dashboard/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\Dashboard\DashboardController;
+use App\Http\Controllers\Dashboard\UserController;
+use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group(['prefix' => LaravelLocalization::setLocale()], function()
+{
+    Route::name('dashboard.')->prefix('dashboard')->group(function (){
+        Route::get('/index',[DashboardController::class,'index'])->name('welcome');
+        Route::resource('/users',UserController::class)->except('show');
+    });
+
+});
+
+
